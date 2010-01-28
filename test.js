77cc41f78eacbc4fe3f8a1f6c5aa4ba2e92c2ef3
Tim Caswell <tim@creationix.com>
2010-01-28 00:53:10 -0600

    Add in some smart wrapping to the pretty printer.

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var Jack = require('jack').Jack;
 var File = require('file');
 process.mixin(require('sys'));
 
-File.read('control_flow.jack').addCallback(function (code) {
+File.read('test.jack').addCallback(function (code) {
   puts(Jack.compile(code));
 });
